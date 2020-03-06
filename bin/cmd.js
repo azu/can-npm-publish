e0be1fa91fa1b7b572d8ceda056fd39db3d4ac3b
Technote <technote.space@gmail.com>
2020-03-07 01:29:20 +0900

    fix: typo (pacakge => package)

--- bin/cmd.js
+++ bin/cmd.js
@@ -5,7 +5,7 @@ const canNpmPublish = require("../lib/can-npm-publish").canNpmPublish;
 const cli = meow(
     `
     Usage
-      $ can-npm-publish [directory|pacakge.json path]
+      $ can-npm-publish [directory|package.json path]
 
     Options
       --verbose  show detail of errors
