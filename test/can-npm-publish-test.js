de602017f63ec862522c195f8b0f9f4042ddba03
azu <azuciao@gmail.com>
2021-06-11 10:41:39 +0900

    fix(deps): update dependencies

--- test/can-npm-publish-test.js
+++ test/can-npm-publish-test.js
@@ -7,30 +7,30 @@ const shouldNotCalled = () => {
 };
 describe("can-npm-publish", () => {
     it("should be rejected, it is invalid name", () => {
-        return canNpmPublish(path.join(__dirname, "fixtures/invalid-name.json")).then(shouldNotCalled, error => {
-            assert.ok(/Invalid name/s.test(error.message));
+        return canNpmPublish(path.join(__dirname, "fixtures/invalid-name.json")).then(shouldNotCalled, (error) => {
+            assert.ok(/name can only contain URL-friendly characters/s.test(error.message));
         });
     });
     it("should be rejected, it is private:true", () => {
-        return canNpmPublish(path.join(__dirname, "fixtures/private.json")).then(shouldNotCalled, error => {
+        return canNpmPublish(path.join(__dirname, "fixtures/private.json")).then(shouldNotCalled, (error) => {
             assert.ok(/This package is private/.test(error.message));
         });
     });
     it("should be rejected, it is already published", () => {
-        return canNpmPublish(path.join(__dirname, "fixtures/already-published.json")).then(shouldNotCalled, error => {
+        return canNpmPublish(path.join(__dirname, "fixtures/already-published.json")).then(shouldNotCalled, (error) => {
             assert.ok(/is already published/.test(error.message));
         });
     });
     it("should be rejected, it is already published to yarnpkg registry", () => {
         return canNpmPublish(path.join(__dirname, "fixtures/already-published-registry.json")).then(
             shouldNotCalled,
-            error => {
+            (error) => {
                 assert.ok(/is already published/.test(error.message));
             }
         );
     });
     it("should be rejected, it is already published scoped package", () => {
-        return canNpmPublish(path.join(__dirname, "fixtures/scoped-package.json")).then(shouldNotCalled, error => {
+        return canNpmPublish(path.join(__dirname, "fixtures/scoped-package.json")).then(shouldNotCalled, (error) => {
             assert.ok(/is already published/.test(error.message));
         });
     });
