7271d435c1e65ba72c14447bcb3b73937f1edc7c
azu <azuciao@gmail.com>
2021-06-11 10:43:06 +0900

    style: format

--- test/can-npm-publish-bin-test.js
+++ test/can-npm-publish-bin-test.js
@@ -10,29 +10,29 @@ const shouldNotCalled = () => {
 };
 
 describe("can-npm-publish bin", () => {
-    it("should return 0, it can publish", done => {
+    it("should return 0, it can publish", (done) => {
         const bin = spawn("node", [binPath, path.join(__dirname, "fixtures/not-published-yet.json")]);
 
         // Finish the test when the executable finishes and returns 0
-        bin.on("close", exit_code => {
+        bin.on("close", (exit_code) => {
             assert.ok(exit_code === 0);
             done();
         });
     });
-    it("should return 1, it can't publish", done => {
+    it("should return 1, it can't publish", (done) => {
         const bin = spawn("node", [binPath, path.join(__dirname, "fixtures/already-published.json")]);
 
         // Finish the test when the executable finishes and returns 1
-        bin.on("close", exit_code => {
+        bin.on("close", (exit_code) => {
             assert.ok(exit_code === 1);
             done();
         });
     });
-    it("should send errors to stderr when verbose, it can't publish", done => {
+    it("should send errors to stderr when verbose, it can't publish", (done) => {
         const bin = spawn("node", [binPath, path.join(__dirname, "fixtures/already-published.json"), "--verbose"]);
 
         // Finish the test and stop the executable when it outputs to stderr
-        bin.stderr.on("data", data => {
+        bin.stderr.on("data", (data) => {
             assert.ok(/almin@0.15.2 is already published/.test(data));
             bin.kill();
             done();
